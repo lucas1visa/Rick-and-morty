b2415b69d4b84f78daef0ab223adbd20fa2cfc78
lucas1visa <visalucas557@gmail.com>
2023-06-02 10:16:18 -0300

    Actualizacion M4

---
+++ Server/src/controllers/postUser.js
@@ -0,0 +1,5 @@
+const {User} = require('../DB_connection')
+
+const postUser = async(req, res)=>{
+    const {email, password} = req.body
+}
--- Server/src/utils/users.js
+++
@@ -1,1 +0,0 @@
-module.exports = [{email:'lucasjulian@gmail.com' , password :12345678}]
